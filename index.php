583f16c80420639e3cbcad4ca900483574202e9c
francesco <cesaranofrancescomain@gmail.com>
2023-01-25 10:30:31 +0100

    creata struttura html

--- index.php
+++ index.php
@@ -1,74 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php 
+    require_once 'models/movie.php';
+    require_once 'db.php';
+    ?>
+</head>
+<body>
+    <h1>MOVIES</h1>
 <?php 
-
-class Movie {
-    private $title;
-    private $year;
-    private $director;
-    private $rating;
-    private $genres;
-
-    public function __construct($title, $year, $director, $rating, $genres) {
-        $this->title = $title;
-        $this->year = $year;
-        $this->director = $director;
-        $this->rating = $rating;
-        $this->genres = $genres;
+    echo "<ul>";
+    foreach ($movies as $movie) {
+        echo "<li>".$movie->getMovieData()."</li>";
     }
-
-    public function getTitle() {
-        return $this->title;
-    }
-
-    public function setTitle($title) {
-        $this->title = $title;
-    }
-
-    public function getYear() {
-        return $this->year;
-    }
-
-    public function setYear($year) {
-        $this->year = $year;
-    }
-
-    public function getDirector() {
-        return $this->director;
-    }
-
-    public function setDirector($director) {
-        $this->director = $director;
-    }
-
-    public function getRating() {
-        return $this->rating;
-    }
-
-    public function setRating($rating) {
-        $this->rating = $rating;
-    }
-
-    public function getGenres() {
-        return $this->genres;
-    }
-
-    public function setGenres($genres) {
-        $this->genres = $genres;
-    }
-    public function getMovieData() {
-        $data = "Title: " . $this->getTitle() . "\nYear: " . $this->getYear() . "\nDirector: " . $this->getDirector() . "\nRating: " . $this->getRating() . "\nGenres: ";
-        foreach($this->getGenres() as $genre) {
-            $data .= $genre . ",";
-        }
-        return $data;
-    }
-}
+    echo "</ul>";
+    ?>  
+</body>
+</html>
 
 
-
-require_once 'db.php';
-
-echo "<ul>";
-foreach ($movies as $movie) {
-    echo "<li>".$movie->getMovieData()."</li>";
-}
-echo "</ul>";
