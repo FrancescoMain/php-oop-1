41ab0a2a05a869a00165d6e10c15f2ddba9632f1
francesco <cesaranofrancescomain@gmail.com>
2023-01-25 10:13:25 +0100

    creato db.pho con database

---
+++ db.php
@@ -0,0 +1,6 @@
+<?php
+$movies = array(
+    new Movie("The Shawshank Redemption", 1994, "Frank Darabont", 9.3, array("Drama","Crime")),
+    new Movie("The Godfather", 1972, "Francis Ford Coppola", 9.2, array("Crime","Drama")),
+    new Movie("The Lord of the Rings: The Return of the King", 2003, "Peter Jackson", 9.0, array("Adventure","Fantasy"))
+);
